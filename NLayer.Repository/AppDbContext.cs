d22534bde00f9de8149a163099170189aaa5f2e4
Onur Güneç <ogunec@hotmail.com>
2023-11-12 23:03:34 +0300

    Configurations and Seed datas explained

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+	public class AppDbContext : DbContext
+	{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); //Bu kod ile Configurations içerisinde
+																						   //yer alan tüm Entityler için olan configurationslar
+																						   //dahil edilir.
+			modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+			{
+				Id = 1,
+				Color = "Kırmızı",
+				Height = 100,
+				Width = 200,
+				ProductId = 1
+
+
+			},
+			new ProductFeature()
+			{
+				Id = 2,
+				Color = "Mavi",
+				Height = 300,
+				Width = 500,
+				ProductId = 2
+
+
+			});
+
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+	internal class CategorySeed : IEntityTypeConfiguration<Category>
+	{
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+
+
+			builder.HasData(
+				new Category { Id = 1, Name = "Kalemler" },
+				new Category { Id = 2, Name = "Kitaplar" },
+				new Category { Id = 3, Name = "Defterler" });
+		}
+	}
+}
