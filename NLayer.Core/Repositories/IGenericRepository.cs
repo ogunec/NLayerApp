1c33f1dc61a8098b5d7101a48416c057d333deed
onurgunec <onurgunec@fatih.bel.tr>
2023-11-20 16:17:01 +0300

    Service created

--- NLayer.Core/Repositories/IGenericRepository.cs
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -4,15 +4,15 @@ namespace NLayer.Core.Repositories
 {
 	public interface IGenericRepository<T> where T : class
 	{
-		Task<T> GetByIdAsync(int id);
-		IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
-		IQueryable<T> Where(Expression<Func<T, bool>> expression);
-		Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-		Task AddAsync(T entity);
-		Task AddRangeAsync(IEnumerable<T> entities);
-		void Update(T entity);
-		void Remove(T entity);
-		void RemoveRange(IEnumerable<T> entities);
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
 
-	}
+    }
 }
--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -11,57 +11,62 @@ namespace NLayer.Repository.Repositories
 {
 	public class GenericRepository<T> : IGenericRepository<T> where T : class
 	{
-		protected readonly AppDbContext _context;
-		private readonly DbSet<T> _dbSet;
-		public GenericRepository(AppDbContext context)
-		{
-			_context = context;
-			_dbSet = _context.Set<T>();
-		}
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
 
-		public async Task AddAsync(T entity)
-		{
-			await _dbSet.AddAsync(entity);
-		}
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
 
-		public async Task AddRangeAsync(IEnumerable<T> entities)
-		{
-			await _dbSet.AddRangeAsync(entities);
-		}
+        public async Task AddAsync(T entity)
+        {
 
-		public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
-		{
-			return await _dbSet.AnyAsync(expression);
-		}
+            await _dbSet.AddAsync(entity);
 
-		public IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
-		{
-			return _dbSet.AsNoTracking().AsQueryable(); // AsNoTracking dediğimizde çekilen veriler memory'e alınmaz ve izlenmez. Performans artar.
-		}
 
-		public async Task<T> GetByIdAsync(int id)
-		{
-			return await _dbSet.FindAsync(id);
-		}
+        }
 
-		public void Remove(T entity)
-		{
-			_dbSet.Remove(entity);
-		}
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
 
-		public void RemoveRange(IEnumerable<T> entities)
-		{
-			_dbSet.RemoveRange(entities);
-		}
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
 
-		public void Update(T entity)
-		{
-			_dbSet.Update(entity);
-		}
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
 
-		public IQueryable<T> Where(Expression<Func<T, bool>> expression)
-		{
-			return _dbSet.Where(expression);
-		}
-	}
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
 }
