e6966d1dca5d0a00b0cce0656fb96cf3d2977cf7
Onur Güneç <ogunec@hotmail.com>
2023-11-21 00:17:05 +0300

    AutoMapper explained

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Repositories;
+using NLayer.Core.Services;
 using NLayer.Core.UnitOfWork;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWork;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +20,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
 	x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+	public class MapProfile :Profile
+	{
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap(); // Burada Product nesnesi ProductDto nesnesine dönüştürülürken 
+            CreateMap<Category, CategoryDto>().ReverseMap(); // ReverseMap dediğimiz için tam tersine dönüşebilmektedir.
+			CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
